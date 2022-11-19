7b77aee66a5bd8c690f2212ff2430c565fd046d5
Cynthia Okoduwa <Cynthia.Okoduwa@gmail.com>
2022-11-19 11:43:07 +0100

    update to variables.tf

--- variables.tf
+++ variables.tf
@@ -43,6 +43,31 @@ variable "tags" {
   default     = {}
 }
 
+variable "environment" {
+  type = string
+  description = environment
+}
+
+variable "ami-bastion" {
+  type = string
+  description = "AMI ID for the launch template"
+}
+
+variable "ami-web" {
+  type = string
+  description = "AMI ID for the launch template"
+}
+
+variable "ami-nginx" {
+  type = string
+  description = "AMI ID for the launch template"
+}
+
+variable "ami-sonar" {
+  type = string
+  description = "AMI ID for the launch template"
+}
+
 variable "ami" {
   type        = string
   description = "AMI ID for the launch template"
